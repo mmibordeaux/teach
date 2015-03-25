1ab02123fab7d2ab475f505a76589a49478c430b
Arnaud Levy <arnaud@semiodesign.com>
2015-03-25 08:21:12 +0100

    improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.all.order(:last_name, :first_name)
   end
 
   # GET /users/1
