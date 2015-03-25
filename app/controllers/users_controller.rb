0e969489e4ebdce019d30160342276820ec7c4df
Arnaud Levy <arnaud@semiodesign.com>
2015-03-25 08:12:04 +0100

    improvements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :hours)
+      params.require(:user).permit(:first_name, :last_name, :hours, :tenured)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324195250) do
+ActiveRecord::Schema.define(version: 20150325071140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,7 @@ ActiveRecord::Schema.define(version: 20150324195250) do
     t.integer  "hours"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "tenured"
   end
 
 end
