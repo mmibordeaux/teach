e5292a44c2b1354ddf7326df975db0041e0f2d25
Arnaud Levy <arnaud@semiodesign.com>
2015-03-29 12:45:28 +0200

    unique fixed

--- app/models/teaching_module.rb
+++ app/models/teaching_module.rb
@@ -30,7 +30,7 @@ class TeachingModule < ActiveRecord::Base
   has_many :fields_teaching_modules
   has_many :fields, through: :fields_teaching_modules
   # has_many :projects_teaching_modules # TODO remove table, we are not going to connect them exclusively through the fields
-  has_many :projects, through: :fields, uniq: true
+  has_many :projects, -> { uniq }, through: :fields
   
   accepts_nested_attributes_for :fields_teaching_modules, allow_destroy: true
   accepts_nested_attributes_for :fields
