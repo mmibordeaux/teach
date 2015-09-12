d24404f61c721b8a3bd9e26208bf805ffe271fcd
Arnaud Levy <arnaud@semiodesign.com>
2015-09-12 15:42:23 +0200

    hours by semester

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -13,9 +13,31 @@ class Semester < ActiveRecord::Base
   has_many :teaching_modules
 
   def student_hours
-    student_hours = 0
-    teaching_modules.each { |tm| student_hours += tm.student_hours }
-    student_hours
+    teaching_modules.collect(&:student_hours).sum
+  end
+
+  def student_hours_cm
+    teaching_modules.collect(&:hours_cm).sum.round(2)
+  end
+
+  def student_hours_td
+    teaching_modules.collect(&:hours_td).sum.round(2)
+  end
+
+  def student_hours_tp
+    teaching_modules.collect(&:hours_tp).sum.round(2)
+  end
+
+  def planned_student_hours_cm
+    teaching_modules.collect(&:planned_student_hours_cm).sum.round(2)
+  end
+
+  def planned_student_hours_td
+    teaching_modules.collect(&:planned_student_hours_td).sum.round(2)
+  end
+
+  def planned_student_hours_tp
+    teaching_modules.collect(&:planned_student_hours_tp).sum.round(2)
   end
 
   def to_s
