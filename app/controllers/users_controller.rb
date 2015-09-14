ccd5dc6ab1f2ffbedbad56afa9ebe582c4c052e2
Arnaud Levy <arnaud@semiodesign.com>
2015-09-14 13:52:16 +0200

    public in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,6 +76,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :hours, :tenured, project_ids: [], teaching_module_ids: [], field_ids: [])
+      params.require(:user).permit(:first_name, :last_name, :hours, :public, :tenured, project_ids: [], teaching_module_ids: [], field_ids: [])
     end
 end
