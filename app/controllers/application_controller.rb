7aef2a6cfd40b9f1142559d1e8069695722506d9
Arnaud Levy <arnaud@semiodesign.com>
2017-10-15 19:03:42 +0200

    devise and sendgrid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :authenticate
+  before_action :authenticate_user!
 
   def parse
     render json: PPN.parse
   end
-  
-  protected
-
-    def authenticate
-      if Rails.env.production?
-        authenticate_or_request_with_http_basic do |username, password|
-          username == ENV['MMI_USERNAME'] && password == ENV['MMI_PASSWORD']
-        end
-      end
-    end
-
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,14 +2,25 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  first_name :string
-#  last_name  :string
-#  hours      :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  tenured    :boolean
-#  public     :boolean
+#  id                     :integer          not null, primary key
+#  first_name             :string
+#  last_name              :string
+#  hours                  :integer
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  tenured                :boolean
+#  public                 :boolean
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#  admin                  :boolean          default(FALSE), not null
 #
 
 require 'test_helper'
