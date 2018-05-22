5bdba5ef992ee9558b6d5974b02cd6d79aa8bb7a
Arnaud Levy <alevy@lespoupees.paris>
2018-05-22 09:40:15 +0200

    fr in public

--- app/models/project.rb
+++ app/models/project.rb
@@ -22,7 +22,7 @@ class Project < ActiveRecord::Base
   belongs_to :user
   belongs_to :year
   
-  # TODO validator, position must be 1-52
+  validates_inclusion_of :number, in: 1..52
   accepts_nested_attributes_for :fields, allow_destroy: true
   accepts_nested_attributes_for :semesters, allow_destroy: true
 
