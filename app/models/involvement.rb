5355a622f8f7cfe0b15ccf9a963c67503e8ec327
Arnaud Levy <alevy@lespoupees.paris>
2019-07-30 22:14:08 +0200

    clean

--- app/models/involvement.rb
+++ app/models/involvement.rb
@@ -61,9 +61,9 @@ class Involvement < ActiveRecord::Base
   # Costs
 
   COST_RATIO_CM = 1.5
-  COST_RATIO_TD = 1
+  COST_RATIO_TD = 1.0
   # COST_RATIO_TP = 0.66
-  COST_RATIO_TP = 1
+  COST_RATIO_TP = 1.0
 
   COST_HOUR_PRIVATE = 58.31
   COST_HOUR_PUBLIC = 42.96
