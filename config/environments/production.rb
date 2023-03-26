bb8fa6611252d3c821081dfe3d7c07f333ad97b1
Arnaud Levy <contact@arnaudlevy.com>
2023-03-26 19:06:10 +0200

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,12 +64,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: "teach.mmibordeaux.com" }
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-      api_key: ENV['MAILGUN_API_KEY'],
-      domain: ENV['MAILGUN_DOMAIN'],
-      host: 'api.eu.mailgun.net'
+ 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    address:        ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         'teach.mmibordeaux.com',
+    authentication: :plain,
   }
+
+  # config.action_mailer.delivery_method = :mailgun
+  # config.action_mailer.mailgun_settings = {
+  #     api_key: ENV['MAILGUN_API_KEY'],
+  #     domain: ENV['MAILGUN_DOMAIN'],
+  #     host: 'api.eu.mailgun.net'
+  # }
   
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
